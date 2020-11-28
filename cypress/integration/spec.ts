ff1b4a8d52a4d98b7688fbdfe71ea48f17b0ac2f
Alexander Dolgov <a.n.dolgov@gmail.com>
2020-11-28 23:34:02 +0100

    test: make test failing

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -4,11 +4,6 @@ describe('When Angular starting page is loaded', () => {
   });
 
   it('has app title, shows proper command by default and reacts on command changes', () => {
-    cy.contains('cypress-e2e-testing-angular');
-
-    cy.contains('.terminal', 'ng generate component xyz');
-
-    cy.contains('Angular Material').click();
-    cy.contains('.terminal', 'ng add @angular/material');
+    cy.contains('non existing string');
   });
 });
